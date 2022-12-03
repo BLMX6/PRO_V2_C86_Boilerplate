7c94562881a05be84fa69e940ae1f367c3f8fb57
BLMX6 <119624247+BLMX6@users.noreply.github.com>
2022-12-03 11:20:09 -0600

    Update CreateStory.js

--- screens/CreateStory.js
+++ screens/CreateStory.js
@@ -62,7 +62,7 @@ export default class CreateStory extends Component {
               ></Image>
             </View>
             <View style={styles.appTitleTextContainer}>
-              <Text style={styles.appTitleText}>New Story</Text>
+              <Text style={styles.appTitleText}>Nueva historia</Text>
             </View>
           </View>
           <View style={styles.fieldsContainer}>
@@ -107,7 +107,7 @@ export default class CreateStory extends Component {
               <TextInput
                 style={styles.inputFont}
                 onChangeText={(title) => this.setState({ title })}
-                placeholder={"Title"}
+                placeholder={"Título"}
                 placeholderTextColor="white"
               />
 
@@ -118,7 +118,7 @@ export default class CreateStory extends Component {
                   styles.inputTextBig,
                 ]}
                 onChangeText={(description) => this.setState({ description })}
-                placeholder={"Description"}
+                placeholder={"Descripción"}
                 multiline={true}
                 numberOfLines={4}
                 placeholderTextColor="white"
@@ -130,7 +130,7 @@ export default class CreateStory extends Component {
                   styles.inputTextBig,
                 ]}
                 onChangeText={(story) => this.setState({ story })}
-                placeholder={"Story"}
+                placeholder={"Historia"}
                 multiline={true}
                 numberOfLines={20}
                 placeholderTextColor="white"
@@ -143,7 +143,7 @@ export default class CreateStory extends Component {
                   styles.inputTextBig,
                 ]}
                 onChangeText={(moral) => this.setState({ moral })}
-                placeholder={"Moral of the story"}
+                placeholder={"Moraleja de la historia"}
                 multiline={true}
                 numberOfLines={4}
                 placeholderTextColor="white"
