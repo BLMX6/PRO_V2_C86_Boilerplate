f7092d53784eed63b36cfaf08145897bc96fabf5
BLMX6 <119624247+BLMX6@users.noreply.github.com>
2022-12-03 11:17:11 -0600

    Update DrawerNavigator.js

--- navigation/DrawerNavigator.js
+++ navigation/DrawerNavigator.js
@@ -9,9 +9,9 @@ const Drawer = createDrawerNavigator();
 const DrawerNavigator = () => {
   return (
     <Drawer.Navigator screenOptions={{ headerShown: false }}>
-      <Drawer.Screen name="MyHome" component={StackNavigator} />
-      <Drawer.Screen name="Profile" component={Profile} />
-      <Drawer.Screen name="Logout" component={Logout} />
+      <Drawer.Screen name="Inicio" component={StackNavigator} />
+      <Drawer.Screen name="Perfil" component={Profile} />
+      <Drawer.Screen name="CerrarSesion" component={Logout} />
     </Drawer.Navigator>
   );
 };
